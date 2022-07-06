287207dabb9ea84dce4e41606bf2f77acc599720
David Wögerbauer <56993269+d-woegerbauer@users.noreply.github.com>
2022-07-06 14:49:52 +0200

    Work at atom button

--- frontend/components/atoms/Button/Button.tsx
+++ frontend/components/atoms/Button/Button.tsx
@@ -1,12 +1,58 @@
+import React from "react";
 import { styled } from "../../../stitches.config";
+import type * as Stitches from "@stitches/react";
 
-export const Button = styled("button", {
-  borderRadius: "8px",
+type Props = {
+  size?: Stitches.VariantProps<typeof StyledButton>["size"];
+  disabled?: boolean;
+  leftIcon?: React.ReactNode;
+  rightIcon?: React.ReactNode;
+  children: React.ReactNode;
+};
+
+const StyledButton = styled("button", {
   width: "fit-content",
+  padding: "$spacing-2x $spacing-3x",
   border: "none",
-  padding: "10px 20px",
+  borderRadius: "$border-radius-medium",
 
-  fontSize: "1rem",
+  color: "$neutral-100",
+  backgroundColor: "$brand-200",
   cursor: "pointer",
-  fontWeight: "bold",
+  transition: "all 0.2s ease-in-out",
+
+  "&:hover": {
+    backgroundColor: "$brand-400",
+  },
+
+  "&:disabled": {
+    cursor: "not-allowed",
+    backgroundColor: "$neutral-200",
+    color: "$neutral-500",
+  },
+
+  variants: {
+    size: {
+      medium: {},
+      small: {
+        padding: "$spacing-1x $spacing-3x",
+      },
+    },
+  },
 });
+
+export const Button: React.FC<Props> = ({
+  size = "medium",
+  disabled = false,
+  leftIcon,
+  rightIcon,
+  children,
+}) => {
+  return (
+    <>
+      <StyledButton size={size} disabled={disabled}>
+        {children}
+      </StyledButton>
+    </>
+  );
+};
