0b676ca6d4bf87825cba6c76db211bc60d5f2f73
Tobias Graski <tobias.graski@gmx.at>
2022-07-07 11:23:37 +0200

    Add skills endpoint

---
+++ backend/src/entities/skill.entity.ts
@@ -0,0 +1,34 @@
+import { Exclude } from "class-transformer";
+import { HttpResponse } from "./http-response.entity";
+
+export class SkillResponse implements SkillEntity, HttpResponse {
+    statusCode: number;
+    message: string;
+    error?: string;
+    data?: any;
+    id: string;
+    name: string;
+    skillMatrix: SkillRating[];
+
+    constructor(partial: Partial<SkillResponse>) {
+        Object.assign(this, partial);
+    }
+
+}
+
+export class SkillEntity {
+    @Exclude()
+    id: string;
+
+    name: string;
+    skillMatrix: SkillRating[]
+
+    constructor(partial: Partial<SkillEntity>) {
+        Object.assign(this, partial);
+    }
+}
+
+export class SkillRating {
+    rating: number;
+    aggregation: string;
+} 
--- backend/src/skill/skill.controller.ts
+++ backend/src/skill/skill.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, UseGuards } from '@nestjs/common';
+import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
+import { SkillResponse } from 'src/entities/skill.entity';
+import { SkillService } from './skill.service';
+@Controller('/api/skill')
+export class SkillController {
+    constructor(private readonly skillService: SkillService) { }
 
-@Controller('skill')
-export class SkillController {}
+    // @UseGuards(JwtAuthGuard)
+    @Get()
+    async getAll(): Promise<SkillResponse> {
+        return await this.skillService.findAll();
+    }
+}
