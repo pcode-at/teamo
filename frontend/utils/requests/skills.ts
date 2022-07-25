c7bc26a74c354def712260ac6372ca85d36ca68f
Florian <floriandoppler@icloud.com>
2022-07-25 13:31:45 +0200

    Work on create & manage skills

--- frontend/utils/requests/skills.ts
+++ frontend/utils/requests/skills.ts
@@ -3,3 +3,10 @@ import { fetchData } from "./default";
 export async function getSkills() {
   return fetchData("skill", "GET", 200);
 }
+
+export async function createSkill(skill: {
+  name: string;
+}){
+  console.log(skill);
+  return fetchData("skill", "POST", 201, skill);
+}
